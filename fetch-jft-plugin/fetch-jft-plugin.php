6903ecf6a8159d0e4e0587fd92346c99b1784646
pjaudiomv <pjaudiomv@gmail.com>
2018-04-19 19:34:17 -0400

    added settings page

--- fetch-jft-plugin/fetch-jft-plugin.php
+++ fetch-jft-plugin/fetch-jft-plugin.php
@@ -4,7 +4,7 @@ Plugin Name: Fetch JFT
 Plugin URI: https://wordpress.org/plugins/fetch-jft/
 Author: Patrick J NERNA
 Description: This is a plugin that fetches the Just For Today from NAWS and puts it on your site Simply add [jft] shortcode to your page. Fetch JFT Widget can be added to your sidebar or footer as well.
-Version: 1.1.1
+Version: 1.2.0
 Install: Drop this directory into the "wp-content/plugins/" directory and activate it.
 */
 /* Disallow direct access to the plugin file */
@@ -12,28 +12,92 @@ if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
 	die('Sorry, but you cannot access this page directly.');
 }
 
+require_once('simple_html_dom.php');
+
+add_action('admin_menu', function() {
+    add_options_page( 'Fetch JFT Plugin Settings', 'Fetch JFT', 'manage_options', 'jft-plugin', 'fetch_jft_plugin_page' );
+});
+
+add_action( 'admin_init', function() {
+    register_setting( 'jft-plugin-settings-group', 'jft_layout' );
+});
+
+function fetch_jft_plugin_page() {
+ ?>
+   <div class="wrap">
+     <form action="options.php" method="post">
+       <?php
+       settings_fields( 'jft-plugin-settings-group' );
+       do_settings_sections( 'jft-plugin-settings-group' );
+       ?>
+  <h3>Fetch JFT Layout</h3>
+  <p>Chnage between a plain table and css.</p>
+			<table class="form-table">
+		        <tr valign="top">
+		        	<th scope="row">Layout</th>
+		        	<td>
+		        		<select id="jft_layout" name="jft_layout">
+							<option value="table" <?php if(esc_attr(get_option('jft_layout'))=='table') echo 'selected="selected"'; ?>>Table</option>
+							<option value="block" <?php if(esc_attr(get_option('jft_layout'))=='block') echo 'selected="selected"'; ?>>Block</option>
+						</select>
+		        	</td>
+		        </tr>
+		    </table>
+  <?php  submit_button(); ?>
+  </form>
+   </div>
+ <?php
+}
+
+// add settings link to plugins page
+function plugin_add_settings_link( $links ) {
+    $settings_link = '<a href="options-general.php?page=fetch-jft-plugin">' . __( 'Settings' ) . '</a>';
+    array_push( $links, $settings_link );
+  	return $links;
+}
+$plugin = plugin_basename( __FILE__ );
+add_filter( "plugin_action_links_$plugin", 'plugin_add_settings_link' );
+
+
 function jft_func( $atts ){
-
-	$d = new DOMDocument;
-
-	$jft = new DOMDocument;
-	
 	// Get the contents of JFT
-	$d->loadHTML(file_get_contents('https://jftna.org/jft/'));
-
-	// Parse and extract just the body
-	$body = $d->getElementsByTagName('body')->item(0);
-
-	foreach ($body->childNodes as $child) {
-
-		$jft->appendChild($jft->importNode($child, true));
+	if(get_option(jft_layout) == 'block'){
+		$d = file_get_html('https://jftna.org/jft/');
+		$jft_ids = array('jft-date','jft-title','jft-page','jft-quote','jft-quote-source','jft-content','jft-thought','jft-copyright');
+		$jft_class = array('wp-jft','wp-jft','wp-jft','wp-jft','wp-jft','wp-jft','wp-jft','wp-jft');
+		$i = 0;
+		foreach($d->find(tr) as $element) {
+			if($i != 5) {
+				$formated_element = trim(strip_tags($element));
+				echo '<div id="'.$jft_ids[$i].'">'.$formated_element.'</div>';
+			}else{
+				$break_array = preg_split('/<br[^>]*>/i', $element);
+				foreach($break_array as $p) {
+					if(!empty($p)){
+						$content = '<p class="'.$jft_ids[$i].'">'.trim($p).'</p>';
+						echo preg_replace("/<p[^>]*>([\s]|&nbsp;)*<\/p>/", '', $content); 
+					}
+				}
+			}
+			$i++; 
+		}
+	}else{
+		$d = new DOMDocument;
+		$jft = new DOMDocument;
+		
+		// Get the contents of JFT
+		$d->loadHTML(file_get_contents('https://jftna.org/jft/'));
+		// Parse and extract just the body
+		$body = $d->getElementsByTagName('body')->item(0);
+		foreach ($body->childNodes as $child) {
+			$jft->appendChild($jft->importNode($child, true));
+		}
+		// export just the html of body
+		echo $jft->saveHTML();
 	}
-	// export just the html of body
-	echo "<div class=\"jft-div\">";
-	echo $jft->saveHTML(); ?>
+	?>
 	<div align="right"><a href="https://www.jftna.org/jft-subscription.htm" target="_blank">Subscribe</a></div>
 	<?php	
-	echo "</div>";
 }
 
 // create [jft] shortcode
@@ -115,5 +179,4 @@ class JFT_Widget extends WP_Widget {
 	}
 }
 /** END Fetch JFT Widget **/
-
 ?>
